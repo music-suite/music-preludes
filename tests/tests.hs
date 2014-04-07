78829d049c0757fc94b0e788910063ef76c3dcbd
Hans Höglund <hans@hanshoglund.se>
2014-04-07 21:39:35 +0200

    Add regression tests (using tasty-golden) #5
    Adds basic test rig

---
+++ tests/tests.hs
@@ -0,0 +1,28 @@
+
+module Main where
+
+import Test.Tasty
+import Test.Tasty.Golden
+import System.Process
+
+main = defaultMain golden
+
+
+testMusicFile name = testGroup name [
+  testMusicFileAs "mid" name,
+  testMusicFileAs "xml" name,
+  testMusicFileAs "ly"  name
+  ]
+
+testMusicFileAs ext name =
+  goldenVsFile 
+    (name ++ "." ++ ext)
+    (name ++ "." ++ ext)
+    (name ++ "." ++ ext) -- TODO use the golden file
+    (rawSystem "" [] >> return ())
+
+golden = testGroup "Golden" [
+  testMusicFile "articulation_all_accents",
+  testMusicFile "articulation_all_separations",
+  testMusicFile "articulation_legato"
+  ]
