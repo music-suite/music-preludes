1b006a942f31c07fcafb097e3374b9e1cc01ebeb
Hans Höglund <hans@hanshoglund.se>
2014-05-07 22:57:30 +0200

    modified:   examples/phrases.hs

--- examples/phrases.hs
+++ examples/phrases.hs
@@ -7,13 +7,16 @@ import Music.Prelude
 subj  = times 2 $ scat [c,c,d,b_,e,e]^/16
 
 -- Each voice differ slighly in onset etc
-voca v = delay ((4/8)^*v) $ mcatMaybes $ scat $ fmap (\i -> (id $ up (_M2^*i) subj) |> rest^*(15/8)) $ [1..6]
+voca v = delay ((4/8)^*v) $ mcatMaybes $ scat $ fmap (\i -> (id $ up (_M2^*i) subj) |> rest^*(15/8)) 
+  $ [0..3]
 
 -- The 
 music = id
   $ title "Phrases"
   $ composer "Anonymous"
   $ timeSignature (3/8) 
+  $ timeSignatureDuring ((14*3/8) <-> 200) (4/8) 
+  $ over phrases (rotateValues 1)
   $ over (phrases.middleV) (octavesAbove 1) 
   $ over phrases fuse 
   $ rcat $ map voca [0..3]
