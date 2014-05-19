cb0c384419d05da6e40f697d272a0eb473d385ed
Hans Höglund <hans@hanshoglund.se>
2014-05-19 19:15:08 +0200

    modified:   examples/analysis.hs

--- examples/analysis.hs
+++ examples/analysis.hs
@@ -4,11 +4,13 @@
 -- import Control.Lens
 import Control.Arrow
 import Music.Prelude.Basic
+import Data.Colour.Names as Color
 
 withOrigin x = (.-. x)
 mapIf p f = uncurry mplus . first f . mpartition p
 unb = (! 0)
 mark = text "!" -- TODO use color or similar
+-- mark = setColor Color.red
 
 markPerfect   = text "Perfect consonances"   . mapIf (\x -> isPerfectConsonance $ withOrigin c $ unb $ x ^?! pitches) mark
 markImperfect = text "Imperfect consonances" . mapIf (\x -> isImperfectConsonance $ withOrigin c $ unb $ x ^?! pitches) mark
