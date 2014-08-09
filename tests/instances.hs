32b8605f35bf9737398802d4fd4275408405b90d
Hans Höglund <hans@hanshoglund.se>
2014-08-10 00:00:37 +0200

    More instance tests

--- tests/instances.hs
+++ tests/instances.hs
@@ -26,6 +26,8 @@ import Data.Semigroup
 import Control.Monad
 import Control.Applicative
 import Control.Comonad
+import qualified Data.Set as Set
+import qualified Data.Map as Map
 import qualified Data.List
 
 type Checkable  a     = (Eq a, Show a, Arbitrary a)
@@ -91,14 +93,41 @@ _Transformable typ = te .&&. tc .&&. tn
     tc s t x = isForwardSpan s && isForwardSpan t ==> transform (s <> t) x === transform s (transform t $ x .: typ)
     tn s x   = isForwardSpan s                    ==> transform (s <> negateV s) x === x .: typ
 
+{-
+  _duration x = (offset x .-. onset x)
+  onset (delay n a)       = n ^+. onset a
+  offset (delay n a)      = n ^+. offset a
+  duration (stretch n a)  = n * duration a
+-}
 _HasPosition :: (Checkable a, Transformable a, HasPosition a) => a -> Property
-_HasPosition typ = ond .&&. ofd .&&. sd .&&. cd
+_HasPosition typ = eqd .&&. ond .&&. ofd .&&. sd .&&. cd
   where
+    eqd a   = True   ==> _duration a                       === _offset a .-. _onset (a .: typ)
     ond n a = n /= 0 ==> _onset (delay n $ a .: typ)       === _onset a  .+^ n
     ofd n a = n /= 0 ==> _offset (delay n $ a .: typ)      === _offset a .+^ n
     sd n a  = n /= 0 ==> _duration (stretch n $ a .: typ)  === n * _duration a
     cd n a  = n /= 0 ==> _duration (stretch (1/n) $ a .: typ) === (1/n) * _duration a
     -- TODO more general
+
+
+{-
+  _duration (beginning t x) + _duration (ending t x) = _duration x
+  
+  _duration (beginning t x) = t `min` _duration x 
+    iff t >= 0
+-}
+_Splittable :: (Checkable a, Transformable a, Splittable a, HasDuration a) => a -> Property
+_Splittable typ = sameDur .&&. minBegin
+  where
+    sameDur t a  = True   ==> _duration (beginning t a) ^+^ _duration (ending t a) === _duration (a .: typ)
+    minBegin     = True   ==> 1 === 1
+    
+    -- ond n a = n /= 0 ==> _onset (delay n $ a .: typ)       === _onset a  .+^ n
+    -- ofd n a = n /= 0 ==> _offset (delay n $ a .: typ)      === _offset a .+^ n
+    -- sd n a  = n /= 0 ==> _duration (stretch n $ a .: typ)  === n * _duration a
+    -- cd n a  = n /= 0 ==> _duration (stretch (1/n) $ a .: typ) === (1/n) * _duration a
+    -- TODO more general
+
 
 data BadMonoid = BM | BM2
   deriving (Eq, Ord, Show, Typeable)
@@ -150,12 +179,23 @@ instance Arbitrary a => Arbitrary (Stretched a) where
 instance Arbitrary a => Arbitrary (Note a) where
   arbitrary = fmap (view note) arbitrary
 
+instance (Ord a, Arbitrary a) => Arbitrary (Set.Set a) where
+  arbitrary = fmap Set.fromList arbitrary
+
+instance (Ord k, Arbitrary k, Ord a, Arbitrary a) => Arbitrary (Map.Map k a) where
+  arbitrary = fmap Map.fromList $ liftA2 zip arbitrary arbitrary
+
 instance Arbitrary a => Arbitrary (Voice a) where
   arbitrary = fmap (view voice) arbitrary  
 -- instance Arbitrary a => Arbitrary (Chord a) where
   -- arbitrary = fmap (view chord) arbitrary  
 instance Arbitrary a => Arbitrary (Score a) where
   arbitrary = fmap (view score) arbitrary  
+instance Arbitrary a => Arbitrary (Track a) where
+  arbitrary = fmap (view track) arbitrary  
+
+-- instance Arbitrary a => Arbitrary (Reactive a) where
+  -- arbitrary = liftA2 zip arbitrary arbitrary
 
 instance Arbitrary a => Arbitrary (Sum a) where
   arbitrary = fmap Sum arbitrary
@@ -172,6 +212,17 @@ instance Eq a => Eq (Score a) where
   x == y = Data.List.sortBy (comparing (^.era)) (x^.notes) == Data.List.sortBy (comparing (^.era)) (y^.notes)
 instance Show (Score a) where
   show _ = "{{Score}}"
+instance Splittable Integer where
+  split _ x = (x,x)
+instance Splittable Int where
+  split _ x = (x,x)
+instance Splittable Double where
+  split _ x = (x,x)
+instance Splittable Char where
+  split _ x = (x,x)
+instance Splittable a => Splittable [a] where
+  split t = unzipR . fmap (split t)
+unzipR f = (fmap fst f, fmap snd f)
 
 
 -- main = quickCheck $ \() () -> True
@@ -196,6 +247,19 @@ main = defaultMain $ testGroup "Instances" $ [
   I_TEST(_Transformable, Time),
   I_TEST(_Transformable, Duration),
   I_TEST(_Transformable, Span),
+
+  I_TEST(_Transformable, [Time]),
+  I_TEST(_Transformable, [Duration]),
+  I_TEST(_Transformable, [Span]),
+
+  -- I_TEST(_Transformable, Set.Set Time),
+  -- I_TEST(_Transformable, Set.Set Duration),
+  -- I_TEST(_Transformable, Set.Set Span),
+
+  I_TEST(_Transformable, Map.Map Int Time),
+  I_TEST(_Transformable, Map.Map Int Duration),
+  I_TEST(_Transformable, Map.Map Int Span),
+
   I_TEST(_Transformable, Int),
   I_TEST(_Transformable, Double),
   I_TEST(_Transformable, Note Int),
@@ -205,30 +269,80 @@ main = defaultMain $ testGroup "Instances" $ [
   I_TEST(_Transformable, Delayed Int),
   I_TEST(_Transformable, Delayed Double),
 
+  -- TODO how to test "pointwise" for Segment and Behavior
+  -- I_TEST(_Transformable, Reactive Int),
+
   I_TEST(_Transformable, Voice Integer),
   -- I_TEST(_Transformable, Chord Integer),
   I_TEST(_Transformable, Score Integer),
 
-  I_TEST(_Transformable, [Voice Integer]),
+  -- SLOW I_TEST(_Transformable, [Voice Integer]),
   -- I_TEST(_Transformable, [Chord] Integer),
-  I_TEST(_Transformable, [Score Integer]),
+  -- SLOW I_TEST(_Transformable, [Score Integer]),
 
   I_TEST(_HasPosition, Time),
-  -- I_TEST(_HasPosition, Duration),
   I_TEST(_HasPosition, Span),
-  -- I_TEST(_HasPosition, Int),
-  -- I_TEST(_HasPosition, Double),
   I_TEST(_HasPosition, Note Int),
   I_TEST(_HasPosition, Note Double),
-  -- I_TEST(_HasPosition, Stretched Int),
-  -- I_TEST(_HasPosition, Stretched Double),
   I_TEST(_HasPosition, Delayed Int),
   I_TEST(_HasPosition, Delayed Double),
 
   -- I_TEST(_HasPosition, Chord Integer),
   I_TEST(_HasPosition, Score Integer),
+  I_TEST(_HasPosition, Track Integer),
   -- I_TEST(_HasPosition, [Chord Integer]),
   I_TEST(_HasPosition, [Score Integer]),
+  I_TEST(_HasPosition, [Track Integer]),
+
+
+
+
+
+
+
+
+
+
+  -- I_TEST(_Splittable, Time),
+  I_TEST(_Splittable, Duration),
+  I_TEST(_Splittable, Span),
+
+  -- I_TEST(_Splittable, [Time]),
+  -- I_TEST(_Splittable, [Duration]),
+  I_TEST(_Splittable, [Span]),
+
+  -- I_TEST(_Splittable, Set.Set Time),
+  -- I_TEST(_Splittable, Set.Set Duration),
+  -- I_TEST(_Splittable, Set.Set Span),
+
+  -- I_TEST(_Splittable, Map.Map Int Time),
+  -- I_TEST(_Splittable, Map.Map Int Duration),
+  -- I_TEST(_Splittable, Map.Map Int Span),
+
+  -- I_TEST(_Splittable, Int),
+  -- I_TEST(_Splittable, Double),
+  I_TEST(_Splittable, Note Int),
+  I_TEST(_Splittable, Note Double),
+  I_TEST(_Splittable, Stretched Int),
+  I_TEST(_Splittable, Stretched Double),
+  I_TEST(_Splittable, Delayed Int),
+  I_TEST(_Splittable, Delayed Double),
+
+  -- TODO how to test "pointwise" for Segment and Behavior
+  -- I_TEST(_Splittable, Reactive Int),
+
+  I_TEST(_Splittable, Voice Integer),
+  -- I_TEST(_Splittable, Chord Integer),
+  I_TEST(_Splittable, Score Integer),
+
+  -- SLOW I_TEST(_Splittable, [Voice Integer]),
+  -- I_TEST(_Splittable, [Chord] Integer),
+  -- SLOW I_TEST(_Splittable, [Score Integer]),
+
+
+
+
+
  
   I_TEST(_Transformable, Stretched [Note Int])
 
