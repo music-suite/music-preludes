f25f004adf8efe1d273cdd6de714388eccc16211
Hans Höglund <hans@hanshoglund.se>
2014-09-19 19:56:57 +0100

    Clean Scheme examples

--- examples/test2.scm
+++ examples/test2.scm
@@ -4,7 +4,9 @@
 (let (
   (cell1 (compress 16 (scat c cs e)))
   (cell2 (compress 16 (scat fs f e)))
-  ; TODO variadic
-  (sequence (lambda (i s1 s2) (scat s1 (up i s2)))))
+  )
 
-  (sequence m3 (scat cell1 cell2) (scat cell2 cell1)))
+  ; TODO variadic
+  (define (seq i x y) (scat x (up i y)))
+
+  (seq m3 (scat cell1 cell2) (scat cell2 cell1)))
