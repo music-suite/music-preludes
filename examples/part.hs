abecde4c2673081f7ad934a66722660f123d643d
Hans Höglund <hans@hanshoglund.se>
2014-10-12 22:10:00 +0100

    Cleaning

--- examples/part.hs
+++ examples/part.hs
@@ -9,9 +9,6 @@
 import Music.Prelude hiding (open)
 import qualified Music.Score as Score
 
-ensemble :: [Part]
-ensemble = [solo tubularBells] <> (divide 2 (tutti violin)) <> [tutti viola] <> [tutti cello] <> [tutti doubleBass]
-
 withTintin :: (HasPitches' a, Score.Pitch a ~ Behavior Pitch) => Pitch -> Score a -> Score a
 withTintin p x = x <> tintin p x
 
@@ -30,7 +27,7 @@ tintin tonic = pitches . mapped %~ relative tonic tintin'
 tintin' :: Interval -> Interval
 tintin' melInterval 
     | isNegative melInterval = error "tintin: Negative interval"
-    | otherwise = last $ takeWhile (< melInterval) $ tintinStandardNotes
+    | otherwise              = last $ takeWhile (< melInterval) $ tintinStandardNotes
     where
         tintinStandardNotes = concat $ iterate (fmap (+ _P8)) minorTriad
         minorTriad = [_P1,m3,_P5]
@@ -53,18 +50,17 @@ bell :: Score StandardNote
 bell = let
     cue :: Score (Maybe StandardNote)
     cue = stretchTo 1 (rest |> a) 
-    in parts' .~ (ensemble !! 0) $ text "l.v." $ mcatMaybes $ times 40 $ scat [times 3 $ scat [cue,rest], rest^*2]
+    in parts' .~ solo tubularBells $ text "l.v." $ mcatMaybes $ times 40 $ scat [times 3 $ scat [cue,rest], rest^*2]
 
 strings :: Score StandardNote
-strings = strings_vln1 <> strings_vln2 <> strings_vla <> strings_vc <> strings_db
-  where
-    strings_vln1 = clef GClef $ parts' .~ (ensemble !! 1) $ up (_P8^*1)   $ strings_cue
-    strings_vln2 = clef GClef $ parts' .~ (ensemble !! 2) $ up (_P8^*0)   $ stretch 2 strings_cue
-    strings_vla  = clef CClef $ parts' .~ (ensemble !! 3) $ down (_P8^*1) $ stretch 4 strings_cue
-    strings_vc   = clef FClef $ parts' .~ (ensemble !! 4) $ down (_P8^*2) $ stretch 8 strings_cue
-    strings_db   = clef FClef $ parts' .~ (ensemble !! 5) $ down (_P8^*3) $ stretch 16 strings_cue
-
-    strings_cue  = delay (1/2) $ withTintin (down (_P8^*4) $ asPitch a) $ mainSubject
+strings = pcat [
+    parts' .~ violins1     $ up (_P8^*1)   $ stringPart,
+    parts' .~ violins2     $ up (_P8^*0)   $ stretch 2 stringPart,
+    parts' .~ violas       $ down (_P8^*1) $ stretch 4 stringPart,
+    parts' .~ cellos       $ down (_P8^*2) $ stretch 8 stringPart,
+    parts' .~ doubleBasses $ down (_P8^*3) $ stretch 16 stringPart]
+    where
+        stringPart = delay (1/2) $ withTintin (down (_P8^*4) $ asPitch a) $ mainSubject
 
 music :: Score StandardNote
 music = meta $ stretch (3/2) $ {-before 60-} (mempty <> bell <> delay 6 strings)
