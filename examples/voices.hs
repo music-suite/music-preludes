bfb8b2a6d2c8e3f669233fbbc531dfed46c646f6
Hans Höglund <hans@hanshoglund.se>
2014-12-07 14:27:29 +0000

    Add voices example

---
+++ examples/voices.hs
@@ -0,0 +1,16 @@
+
+import Music.Prelude
+import qualified Music.Score
+
+subj :: Voice Pitch
+subj = mconcat [c',ab,db',e,f,g,ab,bb,c']
+
+type Chorale = [Voice (Maybe Pitch)]
+
+renderVoice :: Voice (Maybe Pitch) -> Score StandardNote
+renderVoice = fmap pitchToNote . mcatMaybes . renderAlignedVoice . aligned 0 0
+  where
+    pitchToNote = fromPitch' . pure
+
+renderChorale :: Chorale -> Score StandardNote
+renderChorale = pcat . fmap renderVoice
